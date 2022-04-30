cb1fe97139a132144bbc2d3575953da65ed88c19
Mixhetl <75274781+Mixhetl@users.noreply.github.com>
2022-04-30 08:05:13 -0500

    Add objeto console.js

---
+++ 24.- objeto console.js
@@ -0,0 +1,12 @@
+//! OBJETO CONSOLE
+
+//? El objeto console:
+console.log(console);
+console.error("Esto es un error");
+console.warn("Esto es un aviso");
+console.info("Esto es un mensaje informativo");
+console.log("Un registro de lo que ha pasado en nuestra aplicación");
+
+let nombre = "Malquiel";
+let apellido= "Hernández";
+let edad = 35;
