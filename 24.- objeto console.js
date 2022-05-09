5cc6d5d4cff70a12f14216ff750d7eecd1987ec6
Mixhetl <75274781+Mixhetl@users.noreply.github.com>
2022-05-09 17:26:58 -0500

    Update objeto console.js

--- 24.- objeto console.js
+++ 24.- objeto console.js
@@ -36,3 +36,56 @@ console.log("Curso de HTML semantico avanzado.")
 console.log("Curso de React.js");
 console.log("Diseño de Interfaces");
 console.groupEnd();
+
+console.groupCollapsed("Los cursos de @mixhetl en Youtube.");
+console.log("Curso de Javascript");
+console.log("Curso de CSS avanzado");
+console.log("Curso de NODE.js");
+console.log("Curso de HTML semantico avanzado.")
+console.log("Curso de React.js");
+console.log("Diseño de Interfaces");
+console.groupEnd();
+console.clear();
+
+console.table(Object.entries(console).sort());
+console.clear();
+
+const numeros = [1,2,3,4,5];
+const vocales = ["a","e","i","o","u"];
+
+console.table(numeros);
+console.table(vocales);
+
+const perro = {
+    nombre: "Lucas",
+    raza: "pastor",
+    color: "Café"
+}
+console.table(perro);
+console.clear();
+
+//Temporizador
+console.time("Timer");
+const arreglo = Array(1000000);
+
+    for(let i = 0; i<arreglo.length; i++){
+        arreglo[i] = i;
+    }
+
+console.timeEnd("Timer");
+console.clear();
+
+//Impresión del numero de ejecuciones
+for(let i = 0; i <= 100; i++) {
+    console.count("Contador");
+
+    console.log(i);
+}
+console.clear();
+
+//Pruebas
+let x = 3;
+let y = 2;
+let pruebaXY = "Se espera que X siempre sea menor que Y";
+
+console.assert(x < y, (x,y,pruebaXY));
