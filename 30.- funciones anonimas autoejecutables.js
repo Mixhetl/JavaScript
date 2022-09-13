d10056b085a963a0f0c2256a5ab7b1323eea1aed
Mixhetl <75274781+Mixhetl@users.noreply.github.com>
2022-09-13 15:55:13 -0500

    General Update

--- 30.- funciones anonimas autoejecutables.js
+++ 30.- funciones anonimas autoejecutables.js
@@ -1,2 +1,28 @@
 //! FUNCIONES ANONIMAS AUTOEJECUTABLES
 
+(function() {
+    console.log("Mi primer IIFE");
+})();
+
+(function(d, w, c) {
+    console.log("Mi segunda IIFE");
+    console.log(d);
+    console.log(w);
+    c.log("Este es un console.log desde la variable asignada")
+})(document, window, console);
+
+// ? Formas de escribir las Funciones Anonimas Autoejecutables
+// * Clasica
+(function() {
+    console.log("Forma clasica");
+})();
+
+// * La Crockford (JavaScript The God Parts)
+((function() {
+    console.log("Versión Crockford");
+})());
+
+// * Unaria
++function() {
+    
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript
 
 Hola a todos. Estos son mis apuntes de Javascript. Si te son utiles puedes usarlos.
-Si gustas hacer un comentario mucho te lo agradecería. 
+Si gustas hacer un comentario mucho te lo agradecería.
 
 Estoy aprendiendo y todo comentario es bienvenido.
